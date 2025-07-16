2f34b70932fcca0869cdaaf5ea307004ce4ada29
Shashank Mangal <127585673+Shashankmangal1@users.noreply.github.com>
2025-07-16 20:02:01 +0530

    🖱️ Add click reveal interaction to grid cells

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-const board = document.getElementById('game-board');
+const board = document.getElementById("game-board");
 
 const rows = 9;
 const cols = 9;
 
 for (let i = 0; i < rows * cols; i++) {
-  const cell = document.createElement('div');
-  cell.classList.add('cell');
-  board.appendChild(cell);
+  const cell = document.createElement("div"); // add's a new div
+  cell.classList.add("cell"); // add 'cell' class
+
+  //Cell click logic
+
+  cell.addEventListener("click", () => {
+    cell.style.backgroundColor = "#eee"; // Reveal color
+    cell.innerText = 0; //Place holder
+  });
+
+  board.appendChild(cell); // update the board
 }
